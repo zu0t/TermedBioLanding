c3ce51c47676644cf4e7f8a38884e91192bb2a75
0boris <23bosmanov@heckgrammar.co.uk>
2025-02-21 11:41:16 +0000

    Added web site

---
+++ build/_app/immutable/chunks/DFw-JoPW.js
@@ -0,0 +1,1 @@
+import{e}from"./D7XkJ6qu.js";e();
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="h-screen w-full bg-black relative flex flex-col gap-2 items-center justify-center">
+    <h2 class="text-white/80 font-semibold text-xl">TermedBio</h2>
+  <h1 class="signature-text text-7xl font-bold">Coming soon.</h1>
+</div>
+<div
+  class="absolute bottom-0 left-0 w-full h-screen bg-radial-[90%_100%_at_50%_100%] from-[#441dba64] to-transparent"
+></div>
